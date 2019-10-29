134276c0888cb1bbf5d5a9a0c2e8a4950c64c89f
Shane <shanemcneil19@gmail.com>
2019-10-29 12:02:11 -0700

    complete drills

---
+++ src/drills.js
@@ -0,0 +1,67 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+    client: 'pg',
+    connection:process.env.DB_URL,
+})
+
+function searchByName(searchTerm) {
+    knexInstance
+        .select('*')
+        .from('shopping_list')
+        .where('name', 'ILIKE', `%${searchTerm}%`)
+        .then(result => {
+            console.log('search by:', { searchTerm })
+            console.log(result)
+        })
+}
+
+searchByName('dogs')
+
+function paginateItems(pageNumber) {
+    const itemsPerPage = 6
+    const offset = itemsPerPage * (pageNumber - 1)
+    knexInstance
+        .select('*')
+        .from('shopping_list')
+        .limit(itemsPerPage)
+        .offset(offset)
+        .then(result => {
+            console.log('Page:', { pageNumber })
+            console.log(result)
+        })
+}
+
+paginateItems(3)
+
+function itemsAddedAfter(daysAgo) {
+    knexInstance
+        .select('*')
+        .from('shopping_list')
+        .where(
+            'date_added',
+            '>',
+            knexInstance.raw(`now() - '?? days'::INTERVAL`, daysAgo)
+        )
+        .then(result => {
+            console.log('added after:', { daysAgo },' days ago')
+            console.log(result)
+        })
+}
+
+itemsAddedAfter(2)
+
+function costPerCategory() {
+    knexInstance
+        .select('category')
+        .sum('price as total')
+        .from('shopping_list')
+        .groupBy('category')
+        .then(result => {
+            console.log('cost per category:')
+            console.log(result)
+        })
+}
+
+costPerCategory()
---
+++ src/practice.js
@@ -0,0 +1,94 @@
+require('dotenv').config()
+const knex = require('knex')
+
+const knexInstance = knex({
+    client: 'pg',
+    connection: process.env.DB_URL,
+})
+
+const qry = knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .where({ name: 'Point of view gun' })
+    .first()
+    // .then(result => {
+        // console.log(result)
+    // })
+    .toQuery()
+
+console.log(qry)
+
+const searchTerm = 'holo'
+
+knexInstance
+    .select('product_id', 'name', 'price', 'category')
+    .from('amazong_products')
+    .where('name', 'ILIKE', `%${searchTerm}%`)
+    .then(result => {
+        console.log(result)
+    })
+
+// Basic search function
+function searchByProduceName(searchTerm) {
+    knexInstance
+        .select('product_id', 'name', 'price', 'category')
+        .from('amazong_products')
+        .where('name', 'ILIKE', `%${searchTerm}%`)
+        .then(result => {
+            console.log(result)
+        })
+}
+  
+searchByProduceName('holo')
+
+
+// pagination function
+function paginateProducts(page) {
+    const productsPerPage = 10
+    const offset = productsPerPage * (page - 1)
+    knexInstance
+        .select('product_id', 'name', 'price', 'category')
+        .from('amazong_products')
+        .limit(productsPerPage)
+        .offset(offset)
+        .then(result => {
+            console.log(result)
+        })
+}
+  
+paginateProducts(2)
+
+// filtering function
+function getProductsWithImages() {
+    knexInstance
+        .select('product_id', 'name', 'price', 'category', 'image')
+        .from('amazong_products')
+        .whereNotNull('image')
+        .then(result => {
+            console.log(result)
+        })
+}
+  
+getProductsWithImages()
+
+function mostPopularVideosForDays(days) {
+    knexInstance
+        .select('video_name', 'region')
+        .count('date_viewed AS views')
+        .where(
+            'date_viewed',
+            '>',
+            knexInstance.raw(`now() - '?? days'::INTERVAL`, days)
+        )
+        .from('whopipe_video_views')
+        .groupBy('video_name', 'region')
+        .orderBy([
+            { column: 'region', order: 'ASC' },
+            { column: 'views', order: 'DESC' },
+        ])
+        .then(result => {
+            console.log(result)
+        })
+}
+  
+mostPopularVideosForDays(30)
